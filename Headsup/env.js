c4f00d29f01aec0d379a57945c1d140600881d2e
Jeyakannan Malayandi <MALAYAJE@legal.regn.net>
2020-07-30 17:27:29 -0400

    api keys are removed

--- Headsup/env.js
+++ Headsup/env.js
@@ -1,9 +1,9 @@
 export const assitantConfig = {
-ASSISTANT_URL: 'https://api.us-south.assistant.watson.cloud.ibm.com/instances/970b8249-a49c-46a6-a2c6-7ac0dd45b7e2/v2/assistants/f857eaab-0a9b-46e3-830b-a82283a859ce/sessions',
-ASSISTANT_ID: 'f857eaab-0a9b-46e3-830b-a82283a859ce',
-ASSISTANT_IAM_APIKEY: 'YtCMS8VM6yV_GGwQ-jn3t0iZP6ByLNCTLSe1Q-JZsQQr',
-ASSISTANT_IAM_URL: 'https://api.us-south.assistant.watson.cloud.ibm.com/instances/970b8249-a49c-46a6-a2c6-7ac0dd45b7e2',
-SKILL_ID: '0e023eef-102a-4b9d-98c5-880efbec52a9',
-USERNAME: 'Auto-generated service credentials',
-PASSWORD: 'YtCMS8VM6yV_GGwQ-jn3t0iZP6ByLNCTLSe1Q-JZsQQr'
+ASSISTANT_URL: '******',
+ASSISTANT_ID: '******',
+ASSISTANT_IAM_APIKEY: '******',
+ASSISTANT_IAM_URL: '******',
+SKILL_ID: '******',
+USERNAME: '******',
+PASSWORD: '******'
 };
